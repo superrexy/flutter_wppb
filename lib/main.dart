0feb1e28f00546db7624b7ff742df41b1766700f
Bintang Rezeka Ramadani <bintangrezeka@gmail.com>
2022-03-01 20:57:58 +0700

    percobaan 2 done

--- lib/main.dart
+++ lib/main.dart
@@ -52,6 +52,14 @@ class DetailPage extends StatelessWidget {
                 ),
               ),
             ),
+            Container(
+              margin: EdgeInsets.all(16),
+              child: const Text(
+                "Museum inside a decommissioned Russian war submarine with tours & an adjacent park with cafes. Clean and well maintaned. Car park cost 10k, entrance fee 15k/person. You can see KRI Pasopati there, it is a russian whiskey class. You can also watch the video about the Indonesian Navy at the building beside the submarine",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16),
+              ),
+            ),
           ]),
         ));
   }
