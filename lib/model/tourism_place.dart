2ee6695ba823a1f2ea3bed0de5ffd51afd763a23
Bintang Rezeka Ramadani <bintangrezeka@gmail.com>
2022-03-08 09:17:18 +0700

    latihan done

--- lib/model/tourism_place.dart
+++ lib/model/tourism_place.dart
@@ -31,4 +31,24 @@ List tourismPlaceList = [
       name: "Patung Suro Boyo",
       location: "Wonokromo",
       imageAsset: "assets/images/patung_suro_boyo.jpg"),
+  TourismPlace(
+      name: "Kampung Arab",
+      location: "Ampel Kejeron, Kec. Semampir",
+      imageAsset: "assets/images/kampung_arab.jpg"),
+  TourismPlace(
+      name: "Food Junction Grand Pakuwon",
+      location: "Jl. Grand Pakuwon, Banjar Sugihan, Kec. Tandes",
+      imageAsset: "assets/images/food_pakuwon.jpg"),
+  TourismPlace(
+      name: "Kebun Binatang Surabaya",
+      location: "Jl. Setail No.1, Darmo, Kec. Wonokromo",
+      imageAsset: "assets/images/kebun_binatang.jpg"),
+  TourismPlace(
+      name: "Atlantis Land",
+      location: "Jl. Sukolilo No.100, Sukolilo Baru, Kec. Bulak",
+      imageAsset: "assets/images/Atlantis-Land.jpg"),
+  TourismPlace(
+      name: "Patung Budha 4 Rupa",
+      location: "Jl. Pantai Ria Kenjeran No.8, Sukolilo Baru, Kec. Bulak",
+      imageAsset: "assets/images/patung-budha-4-wajah.jpg"),
 ];
